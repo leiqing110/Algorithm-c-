90f343c78462a1097e19b4df9d605beb000a2821
leiqing110 <1107604962@qq.com>
2018-04-12 20:03:16 +0800

    更新
    更新

---
+++ 20180412.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        vector<int>::iterator it;
+        vector<vector<int>>::iterator iter;
+        vector<int> vec_tmp;
+        for(iter = array.begin();iter!=vec.end();iter++)
+        {
+            
+        }
+    }
+};
+
